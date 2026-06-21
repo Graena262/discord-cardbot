eff91613cabd288774f88c4cefbb663ba06ac703
Graena262 <915.yukyan@gmail.com>
2026-06-21 10:04:13 +0900

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,6 +4,10 @@ const TOKEN = process.env.TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
 
+if (!TOKEN || !CLIENT_ID || !GUILD_ID) {
+  console.log("ENV missing:", { TOKEN, CLIENT_ID, GUILD_ID });
+}
+
 const commands = [
   new SlashCommandBuilder()
     .setName("card")
