4ca741ad3fa33bfefbc1be9367a4234d2b943e11
Graena262 <915.yukyan@gmail.com>
2026-06-21 09:32:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
+// 1回だけコマンド登録
+require("./deploy-commands.js");
+
 client.once("ready", () => {
   console.log("Bot起動");
 });
